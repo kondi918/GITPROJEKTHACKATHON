1288244cc005a96cb1056d76f69a862c775058f1
AFvTD <alfreddzek123@gmail.com>
2024-09-28 18:04:02 +0200

    Poprawiony mainjs

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import router from './router.js';
 
-createApp(App).mount('#app')
+
+createApp(App)
+.use(router) // Użyj routera
+.mount('#app');
